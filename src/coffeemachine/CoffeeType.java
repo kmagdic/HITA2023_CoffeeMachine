3afd60edc8621ebb0d478ec426d9e9532911f3fe
zoranjuras <cmundir@gmail.com>
2023-10-31 16:29:56 +0100

    Added version author message

---
+++ src/coffeemachine/CoffeeType.java
@@ -0,0 +1,69 @@
+package coffeemachine;
+
+public class CoffeeType {
+
+    private int milkNeeded;
+    private int waterNeeded;
+    private int coffeeBeansNeeded;
+    private int cupsNeeded;
+    private int price;
+    private String name;
+
+
+    public CoffeeType(String name, int waterNeeded, int milkNeeded, int coffeeBeansNeeded, int cupsNeeded, int price) {
+        this.milkNeeded = milkNeeded;
+        this.waterNeeded = waterNeeded;
+        this.coffeeBeansNeeded = coffeeBeansNeeded;
+        this.cupsNeeded = coffeeBeansNeeded;
+        this.price = price;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMilkNeeded() {
+        return milkNeeded;
+    }
+
+    public void setMilkNeeded(int milkNeeded) {
+        this.milkNeeded = milkNeeded;
+    }
+
+    public int getWaterNeeded() {
+        return waterNeeded;
+    }
+
+    public void setWaterNeeded(int waterNeeded) {
+        this.waterNeeded = waterNeeded;
+    }
+
+    public int getCoffeeBeansNeeded() {
+        return coffeeBeansNeeded;
+    }
+
+    public void setCoffeeBeansNeeded(int coffeeBeansNeeded) {
+        this.coffeeBeansNeeded = coffeeBeansNeeded;
+    }
+
+    public int getCupsNeeded() {
+        return cupsNeeded;
+    }
+
+    public void setCupsNeeded(int cupsNeeded) {
+        this.cupsNeeded = cupsNeeded;
+    }
+}
