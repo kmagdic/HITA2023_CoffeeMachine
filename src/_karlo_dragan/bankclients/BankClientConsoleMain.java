523c8e334ea0e01e1835c59cc9098d6a451136dd
Luka Cavarovic <lukcavaro@foi.hr>
2023-11-10 18:31:16 +0100

    maknuo č

--- src/_karlo_dragan/bankclients/BankClientConsoleMain.java
+++ src/_karlo_dragan/bankclients/BankClientConsoleMain.java
@@ -7,7 +7,7 @@ public class BankClientConsoleMain {
     public static void main(String[] args) {
         makeDBConnection("./banksystem.h2");
         createSchema(conn);
-        addUser("Dragan", "Sačer", "dragan@agileway.com", "test123");
+        addUser("Dragan", "Sacer", "dragan@agileway.com", "test123");
 
     }
 
